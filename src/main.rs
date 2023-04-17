82b7a4e33c46627a2ad6ca0ca6a301638993389e
Minwoo Jeong <jeongmwj@gmail.com>
2023-04-17 18:07:36 +0900

    Implement handling the request

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
-        println!("Connection established!");
+        handle_connection(_stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+    println!("{:#?}", http_request);
 }
